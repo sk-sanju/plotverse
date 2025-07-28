855c34b4d41708910b6c8154e3410023273487f5
Sanjay <sanjayskpy1@gmail.com>
2025-07-28 18:20:15 +0530

    update structer and make some changes in v0.0.7

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", encoding="utf-8") as f:
 
 setup(
     name="plotverse",
-    version="0.0.6",
+    version="0.0.7",
     description="Powerful data visualization and analysis library combining Pandas, Matplotlib, and Seaborn.",
     long_description=open("README.md", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
